76bfb56024ae632630558b5f986f3758855a2979
Nick Howard <nick.howard.81@gmail.com>
2025-07-23 16:12:15 +1000

    settings

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-BOM_JSON_URL = "http://www.bom.gov.au/fwo/IDV60901/IDV60901.95874.json"
+BOM_JSON_URL = "http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json"
 OPEN_METEO_FORECAST_URL = (
     "https://api.open-meteo.com/v1/forecast?"
-    "latitude=-37.75&longitude=145.1&daily=temperature_2m_min,temperature_2m_max,"
+    "latitude=-33.86&longitude=151.2&daily=temperature_2m_min,temperature_2m_max,"
     "precipitation_sum&timezone=Australia%2FSydney"
 )
 
