d9bb311b0b6060fe641220b7742122bfbf1121e4
Nick Howard <nick.howard.81@gmail.com>
2025-07-28 11:01:01 +1000

    Swiss railway clock design

--- static/clock.js
+++ static/clock.js
@@ -4,69 +4,84 @@ function drawClock() {
     const radius = canvas.height / 2;
     ctx.translate(radius, radius);
 
-    setInterval(() => {
+    function render() {
+        ctx.clearRect(-radius, -radius, canvas.width, canvas.height);
         drawFace(ctx, radius);
-        drawNumbers(ctx, radius);
+        drawTicks(ctx, radius);
         drawTime(ctx, radius);
-    }, 1000);
+    }
+
+    setInterval(render, 1000);
+    render(); // draw immediately
 }
 
 function drawFace(ctx, radius) {
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = '#111';
+    ctx.fillStyle = '#fff';
     ctx.fill();
-    ctx.strokeStyle = '#76e7ff';
     ctx.lineWidth = 4;
+    ctx.strokeStyle = '#ccc';
     ctx.stroke();
 }
 
-function drawNumbers(ctx, radius) {
-    ctx.font = radius * 0.15 + "px Arial";
-    ctx.fillStyle = '#76e7ff';
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    for (let num = 1; num <= 12; num++) {
-        const ang = num * Math.PI / 6;
-        ctx.rotate(ang);
-        ctx.translate(0, -radius * 0.85);
-        ctx.rotate(-ang);
-        ctx.fillText(num.toString(), 0, 0);
-        ctx.rotate(ang);
-        ctx.translate(0, radius * 0.85);
-        ctx.rotate(-ang);
+function drawTicks(ctx, radius) {
+    for (let i = 0; i < 60; i++) {
+        let angle = i * Math.PI / 30;
+        let length = (i % 5 === 0) ? radius * 0.15 : radius * 0.07;
+        ctx.beginPath();
+        ctx.lineWidth = (i % 5 === 0) ? 4 : 2;
+        ctx.strokeStyle = '#000';
+        ctx.rotate(angle);
+        ctx.moveTo(0, -radius);
+        ctx.lineTo(0, -radius + length);
+        ctx.stroke();
+        ctx.rotate(-angle);
     }
 }
 
 function drawTime(ctx, radius) {
     const now = new Date();
-    const hour = now.getHours() % 12;
-    const minute = now.getMinutes();
-    const second = now.getSeconds();
+    let hour = now.getHours() % 12;
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
 
-    // Hour
-    let hourPos = (hour * Math.PI / 6) +
-                  (minute * Math.PI / (6 * 60)) +
-                  (second * Math.PI / (360 * 60));
-    drawHand(ctx, hourPos, radius * 0.5, 6);
+    // Hour hand
+    let hourAngle = (hour * Math.PI / 6) +
+                    (minute * Math.PI / (6 * 60)) +
+                    (second * Math.PI / (360 * 60));
+    drawHand(ctx, hourAngle, radius * 0.5, 6, '#000');
 
-    // Minute
-    let minutePos = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-    drawHand(ctx, minutePos, radius * 0.75, 4);
+    // Minute hand
+    let minAngle = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minAngle, radius * 0.75, 4, '#000');
 
-    // Second
-    let secondPos = second * Math.PI / 30;
-    drawHand(ctx, secondPos, radius * 0.85, 2, '#f55');
+    // Seconds hand (Mondaine style: thin red with round circle at the end)
+    let secAngle = second * Math.PI / 30;
+    ctx.save();
+    ctx.rotate(secAngle);
+    ctx.beginPath();
+    ctx.strokeStyle = '#e4002b';
+    ctx.lineWidth = 2;
+    ctx.moveTo(0, 10);
+    ctx.lineTo(0, -radius * 0.85);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, -radius * 0.9, 6, 0, 2 * Math.PI);
+    ctx.fillStyle = '#e4002b';
+    ctx.fill();
+    ctx.restore();
 }
 
-function drawHand(ctx, pos, length, width, color = '#76e7ff') {
+function drawHand(ctx, pos, length, width, color) {
+    ctx.save();
     ctx.beginPath();
     ctx.lineWidth = width;
-    ctx.lineCap = "round";
+    ctx.lineCap = "butt";
     ctx.strokeStyle = color;
-    ctx.moveTo(0, 0);
     ctx.rotate(pos);
+    ctx.moveTo(0, 0);
     ctx.lineTo(0, -length);
     ctx.stroke();
-    ctx.rotate(-pos);
+    ctx.restore();
 }
